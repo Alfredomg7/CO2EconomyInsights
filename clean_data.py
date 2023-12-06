d7099922ba92f9596816618b0a40e72ae3c877a0
Alfredomg7 <alfredomg4000@gmail.com>
2023-12-06 12:08:41 -0800

    Refactored data preprocessing to include melting in clean data module for analysis readiness and solve bug in the temperature data cleaning function

--- clean_data.py
+++ clean_data.py
@@ -4,9 +4,9 @@ from helper_functions import *
 
 # File names and their respective indicators
 files_and_indicators = {
-    "co2_data": None,
-    "gdp_data": None,
-    "pop_data": None,
+    "co2_data": "CO2 Emissions",
+    "gdp_data": "GDP",
+    "pop_data": "Population",
     "country_data": None,
     "temp_data": None
 }
@@ -28,10 +28,6 @@ for filename in files_and_indicators.keys():
     print(f"Inspecting {filename}:")
     inspect_data(data)
 
-    # Save the indicator name if available
-    if "Indicator Name" in data.columns:
-        files_and_indicators[filename] = data["Indicator Name"].iloc[0]
-
 # Get a list of valid countries from the country_data
 valid_countries = get_valid_country_codes(datasets["country_data"])
 
@@ -43,7 +39,7 @@ for filename, indicator_name in files_and_indicators.items():
     cleaned_data = None
 
     if indicator_name is not None:
-        cleaned_data = clean_indicators_data(data, valid_countries)
+        cleaned_data = clean_indicators_data(data, valid_countries, indicator_name)
     
     elif filename == "temp_data":
         cleaned_data = clean_temperature_data(data,cleaned_datasets["co2_data"])
--- helper_functions.py
+++ helper_functions.py
@@ -8,7 +8,7 @@ def inspect_data(data):
     # Print a separator line for better readability
     print("-"*220)
 
-def clean_indicators_data(data, valid_countries):
+def clean_indicators_data(data, valid_countries, indicator_name):
     # Generate a list of year strings for the required range
     years = [str(year) for year in range(1990, 2021)]
     # Define the columns to be retained in the cleaned data
@@ -19,7 +19,10 @@ def clean_indicators_data(data, valid_countries):
     clean_data = clean_data.dropna(subset=years, how='all')
     # Filter to include only valid countries
     clean_data = clean_data[clean_data['Country Code'].isin(valid_countries)]
+    # Melt the Dataframe to get a 'Year' column
+    clean_data = melt_data(clean_data, indicator_name)
 
+    # Return cleaned Dataframe
     return clean_data
 
 def clean_country_data(data,valid_countries):
@@ -33,32 +36,41 @@ def clean_country_data(data,valid_countries):
     # Filter to include only valid countries
     clean_data = clean_data[clean_data['Country Code'].isin(valid_countries)]
 
+    # Return cleaned Dataframe
     return clean_data
 
 def clean_temperature_data(data, country_data):
     # Preprocessing and filtering data
     data['Year'] = pd.to_datetime(data['dt']).dt.year
-    data = data[data['Year'].between(1990, 2020)]
+    data = data[data['Year'].between(1990, 2014)]
 
     # Grouping and aggregating data, then pivoting
     pivot_data = data.groupby(['Year', 'Country'])['AverageTemperature']\
                      .mean().unstack('Year').reset_index()
 
     # Merging with country_data to get the country codes
-    cleaned_data = pd.merge(pivot_data, country_data[['Country Name', 'Country Code']],
+    clean_data = pd.merge(pivot_data, country_data[['Country Name', 'Country Code']],
                             left_on='Country', right_on='Country Name', how='left')\
                      .drop(columns='Country')
 
     # Reordering columns
-    cols = ['Country Name', 'Country Code'] + [str(year) for year in range(1990, 2021)]
-    cleaned_data = cleaned_data[cols]
+    cols = ['Country Name', 'Country Code'] + [year for year in range(1990, 2014)]
+    clean_data = clean_data[cols]
 
-    return cleaned_data
+    # Melt the Dataframe to get a 'Year' column
+    clean_data = melt_data(clean_data, "Temperature")
+
+    # Return cleaned Dataframe
+    return clean_data
 
 def get_valid_country_codes(country_data, region_column='Region', country_code_column='Country Code'):
     # Get a list of valid countries codes based on the non-null 'Region' column
     valid_countries = country_data[country_data[region_column].notna()]
     return valid_countries[country_code_column].tolist()
+
+def melt_data(data, value_name, id_vars=["Country Name", "Country Code"], var_name="Year"):
+    melted_data = data.melt(id_vars=id_vars, var_name=var_name, value_name=value_name)
+    return melted_data
 
 
 
