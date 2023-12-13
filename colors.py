9fbcf550095f7fa60068f243bdbc6a3053861f17
Alfredo Muñoz Gutierrez <alfredomg4000@gmail.com>
2023-12-12 21:36:39 -0800

    Created dictionary to customize colors in bubble chart function

---
+++ colors.py
@@ -0,0 +1,7 @@
+
+income_group_colors = {
+    'Low income': 'rgba(230, 25, 75, 0.8)',   # Bright red
+    'Lower middle income': 'rgba(60, 180, 75, 0.8)',  # Vivid green
+    'Upper middle income': 'rgba(0, 130, 200, 0.8)',  # Strong blue
+    'High income': 'rgba(245, 130, 48, 0.8)'  # Bold orange
+}
